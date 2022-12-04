e26d2eece3c295ea7e85ba1a5c8b658bdd72dec4
Aleksandr Khmyrov <sa3@mail.ru>
2022-12-04 13:46:58 +0800

    первый commit

---
+++ Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by Александр Хмыров on 04.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
